321ecfb7a1c2fe259cf370ddbcba5fd462bb8c64
Naxanria <gerbrandnijk@gmail.com>
2020-07-30 14:43:17 +0200

    Mappings and some fixes

--- src/main/java/com/wtbw/mods/decoration/Registrator.java
+++ src/main/java/com/wtbw/mods/decoration/Registrator.java
@@ -61,29 +61,29 @@ public class Registrator extends com.wtbw.mods.lib.Registrator {
 
 
         // COLORED REDSTONE LAMPS
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "blue_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "brown_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "cyan_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "gray_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "green_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "lime_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "light_blue_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "light_gray_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "magenta_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "orange_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "pink_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "purple_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "red_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "white_redstone_lamp");
-        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).func_235838_a_(state -> 15).sound(SoundType.GLASS)), "yellow_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "blue_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "brown_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "cyan_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "gray_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "green_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "lime_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "light_blue_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "light_gray_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "magenta_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "orange_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "pink_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "purple_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "red_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "white_redstone_lamp");
+        register(new RedstoneLampBlock(getBlockProperties(Material.REDSTONE_LIGHT, 0.3F).setLightLevel(state -> 15).sound(SoundType.GLASS)), "yellow_redstone_lamp");
 
         // LAVA BLOCK
-        register(new LavaBlock(getBlockProperties(Material.GLASS,0.3F).sound(SoundType.GLASS).func_235838_a_(state -> 15)), "lava_glass");
-        register(new Block(getBlockProperties(Material.ROCK,1.5F, 6.0F).func_235838_a_(state -> 15)), "lava_stone_brick");
-        register(new SlabBlock(getBlockProperties(Material.ROCK,1.5F, 6.0F).func_235838_a_(state -> 15)), "lava_stone_brick_slab");
-        register(new StairsBlock(ModBlocks.LAVA_STONE_BRICK::getDefaultState, getBlockProperties(Material.ROCK,1.5F, 6.0F).func_235838_a_(state -> 15)),"lava_stone_brick_stairs");
-        register(new WallBlock(getBlockProperties(Material.ROCK,1.5F, 6.0F).func_235838_a_(state -> 15)), "lava_stone_brick_wall");
-        register(new Block(getBlockProperties(Material.ROCK,1.5F, 6.0F).func_235838_a_(state -> 15)), "lava_chiseled_stone_brick");
+        register(new LavaBlock(getBlockProperties(Material.GLASS,0.3F).sound(SoundType.GLASS).setLightLevel(state -> 15)), "lava_glass");
+        register(new Block(getBlockProperties(Material.ROCK,1.5F, 6.0F).setLightLevel(state -> 15)), "lava_stone_brick");
+        register(new SlabBlock(getBlockProperties(Material.ROCK,1.5F, 6.0F).setLightLevel(state -> 15)), "lava_stone_brick_slab");
+        register(new StairsBlock(ModBlocks.LAVA_STONE_BRICK::getDefaultState, getBlockProperties(Material.ROCK,1.5F, 6.0F).setLightLevel(state -> 15)),"lava_stone_brick_stairs");
+        register(new WallBlock(getBlockProperties(Material.ROCK,1.5F, 6.0F).setLightLevel(state -> 15)), "lava_stone_brick_wall");
+        register(new Block(getBlockProperties(Material.ROCK,1.5F, 6.0F).setLightLevel(state -> 15)), "lava_chiseled_stone_brick");
 
         // WATER BLOCK
         register(new WaterBlock(getBlockProperties(Material.GLASS,0.3F).sound(SoundType.GLASS)), "water_glass");
